edced4010809693e8dafe146ed4ae789eb8917ae
jourelokyi <89964993+jourelokyi@users.noreply.github.com>
2021-09-07 21:27:55 +0800

    Create amswer2.py

---
+++ amswer2.py
@@ -0,0 +1,18 @@
+import datetime
+import math
+
+class A:
+  def __init__(self, capacity):
+    self.cache = x()
+    self.capacity = capacity
+
+  def get(self, key):
+    if key not in self.cache
+        return -1
+    else:
+        return weight / math.log(self.now -  )
+        
+  def put(self, key, value, weight):
+    self.cache[key] = value
+    if len(self.cache) > self.capacity:
+        self.cache.popitem
